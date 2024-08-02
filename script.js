84aea0702ede2ae73200e636ad2597d4ce4e5bcf
Matt Miles <matt_miles2@live.co.uk>
2024-08-02 16:27:03 +0000

    Update examples in script file

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-    $("#stream1_btn").click(function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+    $("#stream1_btn").on(function() {
+        $(".stream1").hide(); // can also be done with 'show'
+        $(".stream2").hide('slow');
+        $(".stream3").hide('medium');
+        $(".stream1").hide('fast');
+        $(".stream1").hide(1000);
    });
-   $("#stream2_btn").on("click", function() {
+   $("#stream2_btn").click(function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
